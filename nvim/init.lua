f6fad030e28a7df787f969546b7bf65c76cfb5a8
wenhan <iaxs@live.cn>
2024-07-13 00:00:19 +0800

    add sway/waybar/zshrc/nvim dotfiles

---
+++ nvim/init.lua
@@ -0,0 +1,33 @@
+-- ---------- Neovim基础配置 ----------
+
+-- 插件管理器
+require('config.lazyvim')
+-- 基础设置
+require('config.options')
+-- 按键设置
+require('config.keymaps')
+-- 主题设置
+require('config.colorscheme')
+
+-- ---------- 插件配置 ----------
+
+-- 状态栏
+require("plugins.lualine")
+-- 文件管理
+require("plugins/nvim-tree")
+-- 自动补全
+require("plugins/treesitter")
+-- LSP
+require("plugins/lsp")
+-- cmp
+require("plugins/cmp")
+-- 注释
+require("plugins/comment")
+-- 自动配对
+require("plugins/autopairs")
+-- 缓冲区
+require("plugins/bufferline")
+-- git
+require("plugins/gitsigns")
+-- telescope
+require("plugins/telescope")
---
+++ nvim/lua/config/colorscheme.lua
@@ -0,0 +1,9 @@
+local opt = vim.opt
+
+-- 外观
+opt.termguicolors = true
+opt.signcolumn = "yes"
+vim.cmd[[colorscheme tokyonight-moon]]
+
+-- Neovide Config
+vim.g.neovide_transparency = 0.8
